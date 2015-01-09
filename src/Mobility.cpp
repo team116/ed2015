ba11da5babc649064fc47469d09e2ce5f3598424
Will Carty <cartycrabber@gmai.com>
2015-01-08 22:41:00 -0500

    Some basic mobility functions

--- src/Mobility.cpp
+++ src/Mobility.cpp
@@ -1,10 +1,26 @@
 #include "WPILib.h"
 #include "Ports.h"
 #include "Mobility.h"
+#include "Gyro.h"
 
 Mobility* Mobility::INSTANCE = NULL;
 
 Mobility::Mobility()
+{
+
+}
+
+void Mobility::process()
+{
+
+}
+
+void Mobility::setSpeed(float speed)
+{
+
+}
+
+void Mobility::setDirection(int speed)
 {
 
 }
--- src/Mobility.h
+++ src/Mobility.h
@@ -2,16 +2,19 @@
 #define MOBILITY_H_
 
 #include "WPILib.h"
+#include "Gyro.h"
 
 class Mobility
 {
 public:
 	static Mobility* getInstance();
+	void process();
+	void setSpeed(float speed);
+	void setDirection(int degrees);
 
 private:
 	Mobility();
 	static Mobility* INSTANCE;
-
 };
 
 #endif // MOBILITY_H_
