ac479655a5317b30764451cf4268d9f1c550dd55
rakomwolvesbane <robbiemoore4698@gmail.com>
2015-01-17 09:19:08 -0500

    updated DS ports with new information

--- src/Ports.h
+++ src/Ports.h
@@ -9,18 +9,38 @@ namespace RobotPorts
 namespace DSPorts
 {
 	//joysticks
-	const unsigned int MAIN_JOYSTICK = 1;
+	const unsigned int DRIVER_ONE_JOYSTICK = 1;
+	const unsigned int DRIVER_TWO_JOYSTICK = 2;
 
 	//digital inputs
-	const unsigned int GRAB_BUTTON = 1;
+	const unsigned int LIFTER_PRESET_1 = 1;
+	const unsigned int LIFTER_PRESET_2 = 2;
+	const unsigned int LIFTER_PRESET_3 = 3;
+	const unsigned int LIFTER_PRESET_4 = 4;
+	const unsigned int LIFTER_PRESET_5 = 5;
+	const unsigned int LIFTER_PRESET_6 = 6;
+	const unsigned int STACK_ON_STEP_SWITCH = 7;
+	const unsigned int MOVE_UP_BUTTON = 8;
+	const unsigned int MOVE_DOWN_BUTTON = 9;
+	const unsigned int CAMERA_SELECT_TOGGLE = 10;
 
 	//analog inputs
-	const unsigned int LIFTER_POSITION_SWITCH_UP = 1;
-	const unsigned int LIFTER_POSITION_SWITCH_DOWN = 2;
-	const unsigned int TURN_DIRECTION_KNOB = 3;
+	const unsigned int CONTAINER_RAKE_HEIGHT = 1;
+
+	//digital outputs
+	const unsigned int LIFTER_LED_1 = 1;
+	const unsigned int LIFTER_LED_2 = 2;
+	const unsigned int LIFTER_LED_3 = 3;
+	const unsigned int LIFTER_LED_4 = 4;
+	const unsigned int LIFTER_LED_5 = 5;
+	const unsigned int LIFTER_LED_6 = 6;
+	const unsigned int ON_STEP_LED = 7;
+	const unsigned int OFF_STEP_LED = 8;
+
+
 }
 
 namespace JoystickPorts
 {
-
+	const unsigned int OVERRIDE_BUTTON = 1;
 }
