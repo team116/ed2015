d7385ba69cc9101f58eaed879b548b426160f24b
Will Carty <cartycrabber@gmai.com>
2015-01-17 11:30:36 -0500

    Removed DS Test Code... again

--- src/DS.cpp
+++ src/DS.cpp
@@ -19,52 +19,6 @@ void DS::process()
 {
 	mobility->setDirection(main_joystick->GetX(),main_joystick->GetY());
 	mobility->setRotation(main_joystick->GetTwist());
-
-	/*if(grab_button->Get()){
-		manipulator->grab();
-=======
-//	mobility->setDirection(main_joystick->GetX(),main_joystick->GetY());
-//	mobility->setRotation(turn_direction_knob->Get());
-	mobility->setDirection(main_joystick->GetRawAxis(4), main_joystick->GetRawAxis(5));
-	mobility->setRotation(main_joystick->GetRawAxis(0));
-/*	if(main_joystick->GetRawButton(1))
-	{
-		mobility->runTalon(RobotPorts::REAR_LEFT_MOTOR,0.5);
->>>>>>> Test-Branch
-	}
-	else if(!main_joystick->GetRawButton(1))
-	{
-		mobility->runTalon(RobotPorts::REAR_LEFT_MOTOR,0);
-	}
-	if(main_joystick->GetRawButton(2))
-	{
-		mobility->runTalon(RobotPorts::REAR_RIGHT_MOTOR,0.5);
-	}
-<<<<<<< HEAD
-	else{
-		manipulator->moveToHeight(1);
-=======
-	else if(!main_joystick->GetRawButton(2))
-	{
-		mobility->runTalon(RobotPorts::REAR_RIGHT_MOTOR,0);
-	}
-	if(main_joystick->GetRawButton(3))
-	{
-		mobility->runTalon(RobotPorts::FRONT_LEFT_MOTOR,0.5);
-	}
-	else if(!main_joystick->GetRawButton(3))
-	{
-		mobility->runTalon(RobotPorts::FRONT_LEFT_MOTOR,0);
-	}
-	if(main_joystick->GetRawButton(4))
-	{
-		mobility->runTalon(RobotPorts::FRONT_RIGHT_MOTOR,0.5);
-	}
-	else if(!main_joystick->GetRawButton(4))
-	{
-		mobility->runTalon(RobotPorts::FRONT_RIGHT_MOTOR,0);
->>>>>>> Test-Branch
-	}*/
 }
 
 DS* DS::getInstance()
