7932981937979e8c03d2272b3ab4f973a280a8b2
Ian <lordofthemaracas@gmail.com>
2015-01-27 19:43:02 -0500

    first commit for hardware-testing branch, to give the capability to test individual systems on the robot

--- src/DS.h
+++ src/DS.h
@@ -1,39 +1,16 @@
 #ifndef DS_H_
 #define DS_H_
 
-#include "Mobility.h"
-#include "Manipulator.h"
-#include <CameraServer.h>
-
 class DS
 {
 public:
 	static DS* getInstance();
+
 	void process();
-	void processMobility();
-	void processManipulator();
-	void processLEDS();
 
 private:
 	DS();
 	static DS* INSTANCE;
-	Mobility* mobility;
-	Manipulator* manipulator;
-	Log* log;
-	CameraServer* server;
-
-	Joystick* main_joystick;
-	Joystick* secondary_joystick;
-	Joystick* digitalIO;
-
-	bool override;
-	bool on_step;
-	bool drive_type;
-	bool drive_type_handled;
-
-	void doLevelLEDS(int level);
-
-	const static float LIFTER_BUTTON_CHANGE;
 
 };
 
