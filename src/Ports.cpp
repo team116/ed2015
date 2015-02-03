cecdbb1959921078eeca853f50779e9a0cd2859b
Olivia Strahan <rhealha13@gmail.com>
2015-02-02 21:07:53 -0500

    changed Utils::getCurrentTime() to only give hour:minute:second

--- src/Ports.cpp
+++ src/Ports.cpp
@@ -8,13 +8,21 @@
 #include <ctime>
 
 namespace Utils {
-	time_t now;
-	int convertFromVolts(const float voltage, const int voltage_levels, const float max_voltage) {
-		float output = (voltage / max_voltage) * ((float)voltage_levels-1); // needs to be rounded, never do == w/ decimals
-		return (int)(output + 0.5); // round the value
-	}
-	char* getCurrentTime(){
-		time(&now);
-		return ctime(&now);
-	}
+time_t now;
+int convertFromVolts(const float voltage, const int voltage_levels,
+		const float max_voltage) {
+	float output = (voltage / max_voltage) * ((float) voltage_levels - 1); // needs to be rounded, never do == w/ decimals
+	return (int) (output + 0.5); // round the value
+}
+char* getCurrentTime() {
+	struct tm* time;
+	time_t t;
+	time(&t);
+	time = localtime(&t);
+
+	int hour = time->tm_hour;
+	int min = time->tm_min;
+	int sec = time->tm_sec;
+	return "%i:%i:%i", hour, min, sec;
+}
 }
